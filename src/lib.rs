a8183afea6cc378f5cdcf149834c84848bb1f0b2
Soorria Saruva <soorria.ss@gmail.com>
2024-03-16 13:04:17 +1100

    feat: add parsing for null, true, false

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,8 @@ fn parse_json_string(chars: &Vec<char>, from: usize) -> Result<(usize, String), 
 }
 
 fn parse_json_number(chars: &Vec<char>, from: usize) -> Result<(usize, f64), ParseJSONError> {
+    // If the first char is a minus sign, let's just skip for simplicity
+    // in the loop below
     let mut i = match chars.get(from) {
         Some(ch) if ch == &'-' => from + 1,
         _ => from,
@@ -77,9 +79,26 @@ fn parse_json_number(chars: &Vec<char>, from: usize) -> Result<(usize, f64), Par
     return Ok((i - 1, parsed));
 }
 
+fn parse_json_literal(
+    chars: &Vec<char>,
+    from: usize,
+    literal: &str,
+) -> Result<usize, ParseJSONError> {
+    let text = (from..from + literal.len())
+        .filter_map(|i| chars.get(i))
+        .collect::<String>();
+    let is_null = text == literal;
+    return if is_null {
+        Ok(from + literal.len() - 1)
+    } else {
+        Err(ParseJSONError(
+            format!("Expected {literal} but got {text}").to_string(),
+        ))
+    };
+}
+
 pub fn parse_json(string: &str) -> Result<JSONValue, ParseJSONError> {
     let mut i = 0;
-    let len = string.len();
     let chars = string.chars().collect::<Vec<char>>();
 
     let mut output: Option<JSONValue> = None;
@@ -96,6 +115,18 @@ pub fn parse_json(string: &str) -> Result<JSONValue, ParseJSONError> {
         } else if ch.is_numeric() || ch == &'-' {
             let (end_index, parsed_number) = parse_json_number(&chars, i)?;
             output = Some(JSONValue::Number(parsed_number));
+            i = end_index;
+        } else if ch == &'n' {
+            let end_index = parse_json_literal(&chars, i, "null")?;
+            output = Some(JSONValue::Null);
+            i = end_index;
+        } else if ch == &'t' {
+            let end_index = parse_json_literal(&chars, i, "true")?;
+            output = Some(JSONValue::True);
+            i = end_index;
+        } else if ch == &'f' {
+            let end_index = parse_json_literal(&chars, i, "false")?;
+            output = Some(JSONValue::False);
             i = end_index;
         }
 
@@ -122,6 +153,14 @@ mod tests {
         assert_eq!(
             parse_json_string(&r#""hello\", world!""#.chars().collect(), 0),
             Ok((16, r#"hello\", world!"#.to_string()))
+        );
+    }
+
+    #[test]
+    fn parse_json_string_incomplete_string_err() {
+        assert_eq!(
+            parse_json_string(&r#""hello, world!"#.chars().collect(), 0),
+            Err(ParseJSONError("Missing end quotes for string".to_string()))
         );
     }
 
@@ -153,14 +192,29 @@ mod tests {
     #[test]
     fn parse_json_number_2() {
         assert_eq!(
-            parse_json_number(&r#"-1.2e+3,"#.chars().collect(), 0),
-            Ok((6, -1200f64))
+            parse_json_number(&r#"-1.2E-3,"#.chars().collect(), 0),
+            Ok((6, -0.0012f64))
         );
     }
 
     #[test]
     fn parse_json_just_number() {
         assert_eq!(parse_json(&r#"-1.2e+3"#), Ok(JSONValue::Number(-1200f64)));
+    }
+
+    #[test]
+    fn parse_json_null() {
+        assert_eq!(parse_json("null"), Ok(JSONValue::Null));
+    }
+
+    #[test]
+    fn parse_json_true() {
+        assert_eq!(parse_json("true"), Ok(JSONValue::True));
+    }
+
+    #[test]
+    fn parse_json_false() {
+        assert_eq!(parse_json("false"), Ok(JSONValue::False));
     }
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world! {:?}", "-1.2e+3".parse::<f64>());
+    println!(
+        "Hello, world! {:?}",
+        "-1.2e+3".chars().collect::<Vec<_>>()[0..10]
+            .iter()
+            .collect::<String>()
+    );
 }
