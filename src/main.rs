4e247e1fba981b984f52cb48b8d86de1d2383fae
Soorria Saruva <soorria.ss@gmail.com>
2024-03-17 16:50:08 +1100

    feat: use formatted beeg data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use json_pkler::parse_json;
 
 fn main() {
-    const BENCH_DATA_001: &str = include_str!("../bench_data/004_beeg.json");
+    const BENCH_DATA_001: &str = include_str!("../bench_data/006_beeg_formatted.json");
     let _ = parse_json(BENCH_DATA_001).unwrap();
 }
